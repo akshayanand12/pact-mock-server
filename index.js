80c7d68f761a42a29cbff0ad2db7314ff032c818
Akshay Anand <akshayan@thoughtworks.com>
2020-03-14 23:48:27 +0800

    Add start command

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var pact = require('@pact-foundation/pact-node');
 const path = require('path');
 
 var server = pact.createStub({
-    port: 9991,
+    port: 8080,
     pactUrls: [__dirname + '/pacts/user-consumer-user-provider.json']
 });
 
